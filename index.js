21a7ab77fecde5f1d1062605d4a0bd0215c2b863
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-02-14 23:42:40 -0500

    style: simplify XO config

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const questions = require('./lib/questions');
 const format = require('./lib/format-commit');
 
 const config = _.merge(commitizen.configLoader.load(), {
-  'cz-conventional-commit': {maxSubjectLength: 72, bodyLineLength: 100, emoji: false},
+	'cz-conventional-commit': {maxSubjectLength: 72, bodyLineLength: 100, emoji: false},
 })['cz-conventional-commit'];
 
 module.exports = {
-  prompter(cz, commit) {
-    return cz
-      .prompt(questions({types: types.types, aliases}, config))
-      .then(answers => format(answers, {types: types.types, aliases}, config))
-      .then(commitMessage => commit(commitMessage));
-  },
+	prompter(cz, commit) {
+		return cz
+			.prompt(questions({types: types.types, aliases}, config))
+			.then(answers => format(answers, {types: types.types, aliases}, config))
+			.then(commitMessage => commit(commitMessage));
+	},
 };
--- test/integration.test.js
+++ test/integration.test.js
@@ -8,33 +8,33 @@ import engine from '..';
 
 // eslint-disable-next-line ava/no-cb-test
 test.cb(
-  'Each types and aliases produces a valid (with commitlint) and parsable (with conventional-commits-parser) commit message',
-  t => {
-    const answers = {
-      scope: 'test-scope',
-      subject: 'Commit subject',
-      body: 'this the body \n with 2 parts \n mention @vanduynslagerp',
-      breaking: 'Breaking change description\nand another',
-      issues: 'Fixes #3',
-    };
+	'Each types and aliases produces a valid (with commitlint) and parsable (with conventional-commits-parser) commit message',
+	t => {
+		const answers = {
+			scope: 'test-scope',
+			subject: 'Commit subject',
+			body: 'this the body \n with 2 parts \n mention @vanduynslagerp',
+			breaking: 'Breaking change description\nand another',
+			issues: 'Fixes #3',
+		};
 
-    Object.keys(Object.assign({}, types, aliases)).map(async type => {
-      const commitSpy = spy();
+		Object.keys(Object.assign({}, types, aliases)).map(async type => {
+			const commitSpy = spy();
 
-      await engine.prompter({prompt: stub().resolves(Object.assign({type}, answers))}, commitSpy);
-      const [msg] = commitSpy.firstCall.args;
-      const parsed = parser.sync(msg);
-      const lintRes = await load({extends: ['@commitlint/config-angular']}).then(opts => lint(msg, opts.rules));
+			await engine.prompter({prompt: stub().resolves(Object.assign({type}, answers))}, commitSpy);
+			const [msg] = commitSpy.firstCall.args;
+			const parsed = parser.sync(msg);
+			const lintRes = await load({extends: ['@commitlint/config-angular']}).then(opts => lint(msg, opts.rules));
 
-      t.true(lintRes.valid);
-      t.is(parsed.type, aliases[type] ? aliases[type].type : type);
-      t.is(parsed.scope, answers.scope);
-      t.true(parsed.subject.startsWith(answers.subject));
-      t.is(parsed.notes[0].text, answers.breaking);
-      t.is(parsed.references[0].action, 'Fixes');
-      t.is(parsed.references[0].issue, '3');
-      t.is(parsed.mentions[0], 'vanduynslagerp');
-    });
-    t.end();
-  }
+			t.true(lintRes.valid);
+			t.is(parsed.type, aliases[type] ? aliases[type].type : type);
+			t.is(parsed.scope, answers.scope);
+			t.true(parsed.subject.startsWith(answers.subject));
+			t.is(parsed.notes[0].text, answers.breaking);
+			t.is(parsed.references[0].action, 'Fixes');
+			t.is(parsed.references[0].issue, '3');
+			t.is(parsed.mentions[0], 'vanduynslagerp');
+		});
+		t.end();
+	}
 );
